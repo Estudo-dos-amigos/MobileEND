4fa54c8e94001ab9d7601b8b4950d57d16b14651
Renan <61995540+Renanid@users.noreply.github.com>
2023-09-13 18:33:00 -0300

    Tela inicial login
    Tela inicial login

--- SistemaGereneciamentoTasks/App.js
+++ SistemaGereneciamentoTasks/App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World from mobile! pablo gorila</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handleLogin = () => {
+    // Lógica para autenticar o usuário
+    // Você pode implementar sua própria lógica de autenticação aqui
+    const { username, password } = this.state;
+    if (username === 'seu_usuario' && password === 'sua_senha') {
+      alert('Login bem-sucedido!');
+    } else {
+      alert('Login falhou. Verifique seu nome de usuário e senha.');
+    }
+  };
+
+  handleForgotPassword = () => {
+    // Lógica para recuperar a senha do usuário
+    // Implemente sua própria lógica de recuperação de senha aqui
+    alert('Esqueci minha senha!');
+  };
+
+  handleSignUp = () => {
+    // Navegue para a tela de registro ou execute a lógica de registro
+    // Implemente sua própria lógica de registro aqui
+    alert('Ir para a tela de registro');
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(text) => this.setState({ username: text })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          secureTextEntry={true}
+          onChangeText={(text) => this.setState({ password: text })}
+        />
+        <Button title="Entrar" onPress={this.handleLogin} />
+        <TouchableOpacity onPress={this.handleForgotPassword}>
+          <Text style={styles.forgotPassword}>Esqueci minha senha</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.handleSignUp}>
+          <Text style={styles.signUp}>Cadastrar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  input: {  
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 16,
+    paddingLeft: 8,
+  },
+  forgotPassword: {
+    color: 'blue',
+    marginBottom: 16,
+    padding: 16,
+  },
+  signUp: {
+    color: 'green',
+    padding: 16,
   },
 });
+
+export default LoginScreen;
