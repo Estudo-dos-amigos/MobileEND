4f1bdf0ae36e676d271c61d375f3eecb9413aaf3
Renan <61995540+Renanid@users.noreply.github.com>
2023-09-19 12:35:31 -0300

    Novas telas
    novas telas registrar , recuperar, home, login

---
+++ SistemaGereneciamentoTasks/src/components/RecuperarSenhaScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function RecuperarSenhaScreen() {
+  return (
+    <View>
+      <Text>Tela de Recuperação de Senha</Text>
+    </View>
+  );
+}
+
+export default RecuperarSenhaScreen;
---
+++ SistemaGereneciamentoTasks/src/components/RegistrarScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function RegistrarScreen() {
+  return (
+    <View>
+      <Text>Tela de Registro</Text>
+    </View>
+  );
+}
+
+export default RegistrarScreen;
