7abe4e593a34a0122c8c8ef49e330cca28c783c4
Renan <61995540+Renanid@users.noreply.github.com>
2023-09-20 17:30:11 -0300

    Registro e Login funcionais
    Registro el ogin funcionais

---
+++ SistemaGereneciamentoTasks/src/styles/stylesRegistrar.js
@@ -0,0 +1,35 @@
+import {StyleSheet} from 'react-native';
+const stylesRegistrar = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+  },
+  imagem: {
+    width: 350, 
+    height: 100, 
+    marginTop: 50,
+
+  },
+  imagecontainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 60,
+  },
+
+});
+
+export default stylesRegistrar
